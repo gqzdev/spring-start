03d6ef87bb45dbf6ff7d705b29370456cbbd1f99
gqzdev <gqzdev@gmail.com>
2020-05-07 17:40:42 +0800

    websocket

---
+++ spring-websocket/src/main/java/com/gqzdev/messagingstompwebsocket/MessagingStompWebsocketApplication.java
@@ -0,0 +1,9 @@
+package com.gqzdev.messagingstompwebsocket;
+
+//@SpringBootApplication
+public class MessagingStompWebsocketApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(MessagingStompWebsocketApplication.class, args);
+	}
+}
