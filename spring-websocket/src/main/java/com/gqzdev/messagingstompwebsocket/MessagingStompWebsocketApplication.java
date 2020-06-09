74e3ca4843e9b3f389dbdc415369bab30f55b5b8
gqzdev <gqzdev@gmail.com>
2020-06-09 16:54:14 +0800

    refactoring

--- spring-websocket/src/main/java/com/gqzdev/messagingstompwebsocket/MessagingStompWebsocketApplication.java
+++
@@ -1,9 +0,0 @@
-package com.gqzdev.messagingstompwebsocket;
-
-//@SpringBootApplication
-public class MessagingStompWebsocketApplication {
-
-	public static void main(String[] args) {
-//		SpringApplication.run(MessagingStompWebsocketApplication.class, args);
-	}
-}
